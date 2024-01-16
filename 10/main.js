73df9bd40cb12573ebb08941e3822dd27cc162ea
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 22:08:51 -0800

    Adding Comments

---
+++ 10/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Addition
+const additionResult = 4 + 4;
+console.log("Addition Result: " + additionResult);
+// // Subtraction
+// const subtractionResult = 16 - 8;
+// console.log("Subtraction Result: " + subtractionResult);
---
+++ 10/main.ts
@@ -0,0 +1,7 @@
+// Addition
+const additionResult = 4 + 4;
+console.log("Addition Result: " + additionResult);
+
+// // Subtraction
+// const subtractionResult = 16 - 8;
+// console.log("Subtraction Result: " + subtractionResult);
