bcdeb904c20231c5d79f34f516cccb955a04e0e1
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 21:05:54 -0800

    Famous Quote 01

---
+++ 04/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//  Person Name in Variable
+let famousPerson = "Itachi Uciha";
+// Persone Message in a Variable
+let messagE = "Time doesn't heals anything, it just teaches us how to live with the pain.";
+console.log(`${famousPerson} Once sade, "${messagE}" `);
---
+++ 04/main.ts
@@ -0,0 +1,8 @@
+
+//  Person Name in Variable
+let famousPerson = "Itachi Uciha"
+
+// Persone Message in a Variable
+let messagE = "Time doesn't heals anything, it just teaches us how to live with the pain."
+
+console.log(`${famousPerson} Once sade, "${messagE}" `)
