8252adf9fab2c1a389151518c322ce762db52cd7
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 22:59:59 -0800

    My Own Array

---
+++ 13/main.ts
@@ -0,0 +1,9 @@
+let aboutMe = ["Kounain Al Ibad","Jeddah", "Al-Baik", "Bike", "Yamaha R-1","Porsche 911"];
+
+
+ console.log(`My name is ${aboutMe[0]}`);
+  console.log(`I spend my childhood in ${aboutMe[1]}`);
+   console.log(`My Favroite food is ${aboutMe[2]}`);
+    console.log(`I love to ride ${aboutMe[3]}`);
+     console.log(`My Favroite Bike is ${aboutMe[4]}`);
+      console.log(`My Dream care is ${aboutMe[5]}`);
