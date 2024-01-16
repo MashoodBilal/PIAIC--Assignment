950894f234d59ed9ff8822de080e96d8b9b292cf
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 22:47:38 -0800

    Greetings

---
+++ 12/main.ts
@@ -0,0 +1,9 @@
+let names = ["Hammad", "Kounain", "Ibad", "Ahmed"];
+
+
+let message1 = ("Hello Mr.")
+let message2 = ("We will like to inforum you that Today's class has been cancled")
+ console.log(`${message1} ${names[0]} ${message2}`);
+  console.log(`${message1} ${names[1]} ${message2}`);
+   console.log(`${message1} ${names[2]} ${message2}`);
+    console.log(`${message1} ${names[3]} ${message2}`);
