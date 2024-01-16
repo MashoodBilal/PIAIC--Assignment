4fee9ea9e595de44ff4c84ad42875e95f305b254
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 21:10:33 -0800

    Famous Quote 02

---
+++ 05/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//  Person Name in Variable
+let famous_person = "Eren Yeager";
+// Persone Message in a Variable
+let message = "The world is merciless, and it's also very beautiful";
+console.log(`${famous_person} Once sade, "${message}" `);
