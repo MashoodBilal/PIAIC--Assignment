9832b21c451ecab44f8208de8ccb10d024f54faa
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 22:03:14 -0800

    Favorite Number

---
+++ 09/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let myfavnum = 14;
+let message = (`My favorite number is ${myfavnum} because it's my birth date.`);
+console.log(message);
---
+++ 09/main.ts
@@ -0,0 +1,5 @@
+
+let myfavnum = 14
+let message = (`My favorite number is ${myfavnum} because it's my birth date.`)
+
+console.log(message)
