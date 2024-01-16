0432fb4be850632e12b407be04b2362810d8efcd
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 21:51:34 -0800

    Simply four lines with the number 8

---
+++ 08/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Addition
+console.log(5 + 3);
+// Subtraction:
+console.log(16 - 8);
+// Multiplication:
+console.log(2 * 4);
+// Division:
+console.log(64 / 8);
---
+++ 08/main.ts
@@ -0,0 +1,20 @@
+
+// Addition
+
+console.log(5 + 3)
+
+
+
+// Subtraction:
+
+console.log(16 - 8)
+
+
+// Multiplication:
+
+console.log(2 * 4)
+
+
+// Division:
+
+console.log(64 / 8) 
