85f0a9d75e7fbc54e4cfce84aea89dc7e923a8ef
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 21:37:51 -0800

    The Number Eight:

---
+++ 07/main.ts
@@ -0,0 +1,15 @@
+// Addition
+let additionResult = 4 + 4;
+console.log("Addition Result: " + additionResult);
+
+// Subtraction
+let subtractionResult = 16 - 8;
+console.log("Subtraction Result: " + subtractionResult);
+
+// Multiplication
+let multiplicationResult = 2 * 4;
+console.log("Multiplication Result: " + multiplicationResult);
+
+// Division
+let divisionResult = 64 / 8;
+console.log("Division Result: " + divisionResult);
