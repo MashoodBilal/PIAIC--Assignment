23e22822d1b7805d36f7811a7b0ec24400b5f945
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 21:29:24 -0800

    Stripping Names

---
+++ 06/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Printing Name with whitespace
+let personName = "\t Kounain Al Ibad \n";
+console.log("Name Print with Whitespace:", personName);
+// Printing Name with Stripped
+let stripedname = personName.trim();
+console.log("Stripped Name:", stripedname);
---
+++ 06/main.ts
@@ -0,0 +1,12 @@
+// Printing Name with whitespace
+
+let personName = "\t Kounain Al Ibad \n"
+
+console.log("Name Print with Whitespace:", personName)
+
+
+// Printing Name with Stripped
+let stripedname = personName.trim()
+
+console.log("Stripped Name:", stripedname)
+
