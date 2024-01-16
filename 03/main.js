09b13bda6f2435f421d3c0ce0189a4cefd4416d3
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-15 20:52:53 -0800

    lowercase, uppercase, and titlecase.

---
+++ 03/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let personName = "Kounain Al Ibad";
+// Person Name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+// Person Name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+// Person Name in titlecase
+let words = personName.split(" ");
+let titlecaseName = personName;
+console.log("Titlecase:", titlecaseName);
---
+++ 03/main.ts
@@ -0,0 +1,13 @@
+
+let personName = "Kounain Al Ibad";
+
+// Person Name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+
+// Person Name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+
+// Person Name in titlecase
+let words: string[] = personName.split(" ");
+let titlecaseName = personName;
+console.log("Titlecase:", titlecaseName);
