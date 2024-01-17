4081dc070bce202c7df25b3a703f08096e1a57ba
Kounain Al Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-16 21:10:57 -0800

    Changing Guest List

---
+++ 15/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let guest = [
+    "Hammad",
+    "Mashood",
+    "Momi",
+    "Kounain"
+];
+let myFirstPopped = guest.pop();
+console.log(`${myFirstPopped} can not attend the dinner party`);
+let newguest = (guest.splice(3, 1, "Ahmed"));
+guest.forEach((newguest) => {
+    console.log(`Dear ${newguest}, I will like to invite you to dinner party on Saturday Night`);
+});
