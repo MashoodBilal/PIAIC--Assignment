f4f0f14cd9b4af0d6397f0b70d07622bde6a6f11
Kounain Al Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-16 20:47:39 -0800

    Guest List

---
+++ 14/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let guest = ["Hammad", "Mashood", "Momi", "Kounain"];
+guest.forEach((guestname) => {
+    console.log(`Dear ${guestname}, I will like to invite you to dinner party on Saturday Night`);
+});
---
+++ 14/main.ts
@@ -0,0 +1,10 @@
+let guest = ["Hammad", "Mashood", "Momi", "Kounain"];
+
+
+  guest.forEach((guestname)=>
+  {
+    console.log(`Dear ${guestname}, I will like to invite you to dinner party on Saturday Night`)
+  }
+  
+  
+  )
