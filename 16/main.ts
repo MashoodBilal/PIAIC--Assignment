553ccb4cdb6f7ee41352d7f3aca19e6b004d9853
Kounain Al Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-16 21:32:06 -0800

    More Guests

---
+++ 16/main.ts
@@ -0,0 +1,31 @@
+
+
+let message : string =("I found a bigger dinner table for today.")
+console.log(message)
+
+// Guest Name in Array
+
+let guest = [
+       "Hammad",
+        "Mashood",
+         "Momi",
+          "Kounain"];
+
+// Adding New Name in the Beginning of Array
+
+let newguest = "Wajid"
+guest.unshift(newguest)
+
+// Adding New Name in the Middle  of Array
+
+
+ let newguestinmiddle = (guest.splice(2, 0, "Ahmed",));
+
+
+ 
+
+ guest.forEach((guest)=>
+    {
+      console.log(`Dear ${guest}, I will like to invite you to dinner party on Saturday Night`)
+    }
+ )
