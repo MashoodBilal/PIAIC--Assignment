9591915f656491a4e5d7a3622aa0b5cde369a3e1
MashoodBilal <44563181+MashoodBilal@users.noreply.github.com>
2024-01-17 15:49:46 +0500

    Update main.ts
    Added Types

--- 09/main.ts
+++ 09/main.ts
@@ -1,5 +1,5 @@
 
-let myfavnum = 14
-let message = (`My favorite number is ${myfavnum} because it's my birth date.`)
+let myFavnum:number = 14
+let message:string = (`My favorite number is ${myfavnum} because it's my birth date.`)
 
 console.log(message)
