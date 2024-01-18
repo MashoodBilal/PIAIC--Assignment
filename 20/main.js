57d0ef930e4c5c2a763070c6c680d70bb1a04ff3
Kounain Al Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-17 23:10:47 -0800

    Making a list in an array

---
+++ 20/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Let's create a list of my favorite anime characters.
+Object.defineProperty(exports, "__esModule", { value: true });
+let data = ["Eren Yeager", "Itachi Uciha", "Deku", "Goku", "Naruto", "Tanjiro Kamado"];
+console.log("List of my favorite anime characters");
+for (let list of data) {
+    console.log(list);
+}
---
+++ 20/main.ts
@@ -0,0 +1,15 @@
+
+// Let's create a list of my favorite anime characters.
+
+ let data : string [] =["Eren Yeager", "Itachi Uciha", "Deku", "Goku", "Naruto", "Tanjiro Kamado"]
+
+
+console.log("List of my favorite anime characters")
+
+for(let list of data )
+
+{
+
+console.log(list)
+
+}
