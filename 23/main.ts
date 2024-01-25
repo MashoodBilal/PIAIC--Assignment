41991c6bd1f2060e593de972821e5c46542f3ac7
MK Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-25 10:05:28 +0500

    Conditional Tests

---
+++ 23/main.ts
@@ -0,0 +1,47 @@
+//(5 question of true)
+
+// Test 1
+// let number: number = 5;
+// console.log("Is number equal to 5? I predict True.");
+// console.log(number === 5);
+
+// Test 2
+// console.log("Is number not equal to 15? I predict True.");
+// console.log(number !== 15);
+
+// Test 3
+// console.log("Is number greater than 13? I predict True.");
+// console.log(number > 13);
+
+// Test 4
+// console.log("Is number less than or equal to 20? I predict True.");
+// console.log(number <= 20);
+
+// Test 5
+// console.log("Is number multiplied by 2 equal to 20? I predict True.");
+// console.log(number * 2 === 20);
+
+//(5 questions false)
+
+
+
+// Test 1
+// let car: string = 'Porsche';
+// console.log("Is car equal to 'Porsche'? I predict True.");
+// console.log(car === 'Mazda');
+
+// Test 2
+// console.log("Is car not equal to 'Wrangler'? I predict True.");
+// console.log(car !== 'Wrangler');
+
+// Test 3
+// console.log("Is car length less than 5? I predict False.");
+// console.log(car.length < 5);
+
+// Test 4
+// console.log("Is car length greater than 7? I predict False.");
+// console.log(car.length > 7);
+
+// Test 5
+// console.log("Is car equal to 'Wrangler'? I predict False (case-sensitive comparison).");
+// console.log(car === 'Wrangler');
