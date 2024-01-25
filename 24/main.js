e608c6b3c2eb396b5b4892c260ac942675404599
MK Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-25 10:16:47 +0500

    More Conditional Tests

---
+++ 24/main.js
@@ -0,0 +1,28 @@
+"use strict";
+// 1.(Tests for equality and inequality with strings)
+// const string1: string = "apple";
+// const string2: string = "orange";
+// console.log(string1 === string2);
+// console.log(string1 !== string2);
+Object.defineProperty(exports, "__esModule", { value: true });
+// 2.(Tests using the lower case function)
+// const mixedCaseString: string = "Helloeric";
+// console.log(mixedCaseString.toLowerCase() === "helloeric");
+// 3.(Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to)
+// const num1: number = 10;
+// const num2: number = 5;
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+//4.(Tests using "and" and "or" operators)
+// const Condition1: boolean = true;
+// const Condition2: boolean = false;
+// // console.log(Condition1 && Condition2);
+//  console.log(Condition1 || Condition2);
+// 5.(Test whether an item is in a array)
+// const fruits: string[] = ["Grapes", "Mango", "Banana", "Gava"];
+// console.log(fruits.includes("Grapes"))
+// console.log(fruits.includes("grapes"))
